5e0078b4d1878881eda5fa7cdf4ff4d31f4aa811
root <contrerashdaniel@hotmail.com>
2016-11-25 19:07:59 -0600

    Completed change of time with settimeofday function

--- Clienteudp.c
+++ Clienteudp.c
@@ -7,11 +7,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <sys/time.h>
+#include <errno.h>
+
 
 #define BUFLEN 512  //cuantos datos 
-#define NPACK 1     //cuantos datagramas
-#define PORT 9930   //puerto a usar
+#define NPACK 10  //cuantos datagramas
+#define PORT 9930 //puerto a usar
 #define SRV_IP "127.0.0.1" //Ip del servidor
 
 //función para menejo de errores
@@ -27,6 +28,8 @@ int main(void)
   struct sockaddr_in Servidor;
   int IdSocket, i, slen=sizeof(Servidor);
   char buf[BUFLEN]; //datos
+  const struct timeval *tv;
+  &tv[BUFLEN];//para guardar el timeval del servidor (recibido)
 
   //creamos punto de comunicación, de tipo UDP, SOCK_DGRAM
   if ((IdSocket=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
@@ -44,34 +47,37 @@ int main(void)
     fprintf(stderr, "Error en inet_aton()\n");
     exit(1);
   }
-
-  //mandamos 1 solo datagrama de solicitud
-  // Aqui habia un ciclo for
-  printf("Enviando solicitud al servidor... \n");
-  sprintf(buf, "Datagrama %d\n", NPACK);
-  //mandamos buf con la cadena Datagrama 0.1.2...", al servidor
+  
+  
+  //mandamos un datagrama de solicitud
   if (sendto(IdSocket, buf, BUFLEN, 0, (struct sockaddr*)&Servidor, slen)==-1)
     mensajeError("Error en enviar datagrama");
 
-  const struct timeval *tv;
-  //recibimos el paquete con la fecha local del servidor
-  if (recvfrom(IdSocket, &tv, BUFLEN, 0, (struct sockaddr*)&Servidor, &slen)==-1)
-       mensajeError("Error en recvfrom()");
+  
+  //mandamos n datagramas
+  for (i=0; i<NPACK; i++) {
+    printf("Recibiendo información del servidor %d\n", i);
+    if (recvfrom(IdSocket, &tv, BUFLEN, 0, (struct sockaddr*)&Servidor, &slen)==-1)
+      mensajeError("Error en recvfrom()");
+  }
 
+
+  struct timeval now;
+  int rc;
+  int tmls  = (int) &tv->tv_sec;
+  now.tv_sec = tmls/100;
+  int tmlm = (int) &tv->tv_sec;
+  now.tv_usec = tmlm/1000;
+  
   //cambiamos la fecha y hora del sistema.
-  if(settimeofday(tv,0)!=0){
-
+  rc = settimeofday(&now,0);
+  if(rc==0){
     printf("Fecha y hora cambiadas con exito. \n");
+  }else{
+    printf("settimeofday(): Revise ejecucion con permisos de superusuario, "
+        "errno = %d\n",errno);
   }
-  else
-  {
-    mensajeError("Error al cambiar fecha y hora. Revise que tenga permisos de superusuario.");
-  }
-  time_t tiempo = time(0);
-  struct tm *tlocal2 = localtime(&tiempo);
-  char output[128];
-  strftime(output,128,"%d/%m/%y %H:%M:%S",tlocal2);
-  printf("%s\n",output);
+
   //cerramos punto de comunicación
   close(IdSocket);
   return 0;
--- Servidorudp.c
+++ Servidorudp.c
@@ -8,8 +8,8 @@
 #include <string.h>
 #include <time.h>
 
-#define BUFLEN 512 //tamaño de datos recibidos
-#define NPACK 10    //cuantos paquetes
+#define BUFLEN 512 //ysmaño de datos recibidos
+#define NPACK 10 //cuantos paquetes
 #define PORT 9930  //puerto a usar
 
 //función para manejo de mensaje de error
@@ -21,19 +21,12 @@ void mensajeError(char *s)
 
 int main(void)
 {
-  //estructura para guardar el tiempo local del servidor
-  time_t tiempo = time(0);
-  struct tm *tlocal = localtime(&tiempo);
-  char output[128];
-  strftime(output,128,"%d/%m/%y %H:%M:%S",tlocal);
-  printf("%s\n",output);
-
-  //estructuras para guardar datos de cliente y servidor
+  //estructuras para guardar dastos de cliente y servidor
   struct sockaddr_in Servidor, Cliente;
   int IdSocket, i, slen=sizeof(Cliente);
   char buf[BUFLEN];  //para guardar datos recibidos
 
-  //creamos punto de comunicacion con los datos de UDP, SOCK_DGRAM. y protocolo IP, AF_INET
+  //creamos punto decomunicacion con los datos de UDP, SOCK_DGRAM. y protocolo IP, AF_INET
   if ((IdSocket=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
     mensajeError("Error en socket!!!");
 
@@ -42,26 +35,24 @@ int main(void)
   Servidor.sin_family = AF_INET; //ponemos datos de IP y su puerto y aceptamos datos en cualquier dirección ip local
   Servidor.sin_port = htons(PORT);
   Servidor.sin_addr.s_addr = htonl(INADDR_ANY);
-
   //asociamos el socket ya creado con los datos de la estructura de direccionamiento IP
   if (bind(IdSocket, (struct sockaddr*)&Servidor, sizeof(Servidor))==-1)
       mensajeError("Error en bind");
 
-  // Aqui habia un ciclo for
   //recibimos N paquetes y lo guardamos en el buf, desde el CLIENTE: Cliente
   if (recvfrom(IdSocket, buf, BUFLEN, 0, (struct sockaddr*)&Cliente, &slen)==-1)
-      mensajeError("Error en recvfrom()");
-
-  //mostramos datos recibidos y de quien
-  printf("Recibiendo datos del datagrama desde %s:%d\nData: %s\n", 
-  inet_ntoa(Cliente.sin_addr), ntohs(Cliente.sin_port), buf);
-    
-  // Enviamos el paquete luego de recibir la solicitud desde el cliente.
-  // Creamos una estructura de timeval con el valor de la hora y fecha locales del servidor
-  // para almacenarlas y enviarlas al Cliente.
-  const struct timeval *tv = {mktime(tlocal), 0};
-  if(sendto(IdSocket, &tv, BUFLEN, 0, (struct sockaddr*)&Cliente,slen)==-1)
     mensajeError("Error en enviar datagrama");
+  
+  time_t tiempo = time(0);
+  struct tm *tlocal = localtime(&tiempo);
+  struct tm *tnow = gmtime(&time);
+  const struct timeval *tv = {mktime(tlocal), 0};
+  &tv[BUFLEN]; //datos
+  //mandamos los datos de la hora
+  for (i=0; i<NPACK; i++) {
+    if (sendto(IdSocket, &tv, BUFLEN, 0, (struct sockaddr*)&Cliente, slen)==-1)
+      mensajeError("Error en sendto()");
+  }
 
   //cerramos socket.
   close(IdSocket);
